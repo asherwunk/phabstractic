275efd0025a395fb20dd9122d63f694d9a1920da
Asher Wolfstein <asherwunk@gmail.com>
2016-07-10 06:54:15 +0000

    Added Identity Feature v3.0

---
+++ tests/Phabstractic/Features/Identity.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('src/Phabstractic/Features/Identity.php');
+
+use PHPUnit\Framework\TestCase;
+use Phabstractic\Features;
+
+class TestIdentityClass {
+    use Features\Identity {getNewIdentity as public;}
+    
+    public function changePrefix($prefix) {
+        $this->identityPrefix = $prefix;
+    }
+}
+
+class IdentityFeatureTest extends TestCase
+{
+    public function testIdentityStaticProperty() {
+        $obj1 = new TestIdentityClass();
+        $obj2 = new TestIdentityClass();
+        $obj3 = new TestIdentityClass();
+        
+        $this->assertEquals(1, $obj1->getNewIdentity());
+        $this->assertEquals(2, $obj2->getNewIdentity());
+        $this->assertEquals(3, $obj3->getNewIdentity());
+        $this->assertEquals(4, $obj2->getNewIdentity());
+        
+        return array( $obj2, $obj3 );
+    }
+    
+    /**
+     * @depends testIdentityStaticProperty
+     * 
+     */
+    public function testIdentityFeaturePrefix($arr) {
+        list( $obj2, $obj3 ) = $arr;
+        $obj2->changePrefix('test');
+        $this->assertEquals('test5', $obj2->getNewIdentity());
+        $this->assertEquals('6', $obj3->getNewIdentity());
+    }
+    
+    
+}
