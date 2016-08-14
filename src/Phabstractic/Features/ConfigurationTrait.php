e6a39cfe9b0b26451144e4ca5c4ea1a406d4df85
Asher Wolfstein <asherwunk@gmail.com>
2016-08-14 21:55:46 +0000

    Fixed autoloading

--- src/Phabstractic/Features/ConfigurationTrait.php
+++ src/Phabstractic/Features/ConfigurationTrait.php
@@ -29,7 +29,8 @@
  */
 namespace Phabstractic\Features
 {
-    require __DIR__ . '/../../../vendor/autoload.php';
+    require_once('vendor/autoload.php');
+    
     require_once(realpath( __DIR__ . '/../') . '/falcraftLoad.php');
     
     $includes = array('/Features/Exception/ClassDependencyException.php',
--- src/Phabstractic/Resource/ArrayUtilities.php
+++ src/Phabstractic/Resource/ArrayUtilities.php
@@ -29,7 +29,7 @@
  */
 namespace Phabstractic\Resource
 {
-    require __DIR__ . '/../../../vendor/autoload.php';
+    require_once('vendor/autoload.php');
     
     use Zend\Stdlib;
     
